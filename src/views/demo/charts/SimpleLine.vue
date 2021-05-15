107d4115dcad184ef145ce89b557214d91472a9b
bo.yan <bo.yan@xuncetech.com>
2021-05-15 18:11:07 +0800

    feat(dev): add fund nav table line

--- src/views/demo/charts/SimpleLine.vue
+++ src/views/demo/charts/SimpleLine.vue
@@ -1,12 +1,20 @@
 <template>
   <div ref="chartRef" :style="{ height, width }"></div>
-  <a-button type="primary" :loading="iconLoading" @click="getTestVueInfo"> 点击按钮 </a-button>
+  <!-- <a-button type="primary" :loading="iconLoading" @click="getTestVueInfo"> 点击按钮 </a-button> -->
 </template>
 <script lang="ts">
   import { defineComponent, PropType, ref, Ref, onMounted } from 'vue';
   import { useECharts } from '/@/hooks/web/useECharts';
+  import echarts from '/@/utils/lib/echarts';
   // import { uploadApi } from '/@/api/sys/upload';
-
+  var date = [].fill(0);
+  var data = [].fill(0);
+  for (var i = 1; i < 100; i++) {
+    // const n:number = 1 * i;
+    date.push(i);
+    // data.push(Number(Math.round((Math.random() - 0.5) * 20 + data[i - 1])));
+    data.push(i);
+  }
   export default defineComponent({
     data() {
       return {
@@ -39,18 +47,76 @@
       const { setOptions } = useECharts(chartRef as Ref<HTMLDivElement>);
       onMounted(() => {
         setOptions({
-          // backgroundColor: '#0f375f',
+          title: {
+            left: 'center',
+            text: '净值',
+          },
+          tooltip: {
+            trigger: 'axis',
+          },
+          toolbox: {
+            show: true,
+            feature: {
+              dataZoom: {
+                yAxisIndex: 'none',
+              },
+              dataView: { readOnly: false },
+              restore: {},
+              magicType: { type: ['bar'] },
+              saveAsImage: {},
+            },
+          },
+          legend: {
+            data: ['买入'],
+          },
           xAxis: {
             type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            boundaryGap: false,
+            data: date,
           },
           yAxis: {
             type: 'value',
+            boundaryGap: [0, '100%'],
           },
+          dataZoom: [
+            {
+              type: 'inside',
+              start: 0,
+              end: 10,
+            },
+            {
+              start: 0,
+              end: 10,
+            },
+          ],
           series: [
             {
-              data: [150, 230, 224, 218, 135, 147, 260],
+              name: '模拟数据',
               type: 'line',
+              symbol: 'none',
+              sampling: 'lttb',
+              itemStyle: {
+                color: 'red',
+              },
+              markPoint: {
+                data: [
+                  { type: 'max', name: '最大值' },
+                  { type: 'min', name: '最小值' },
+                ],
+              },
+              areaStyle: {
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  {
+                    offset: 0,
+                    color: 'rgb(255, 158, 68)',
+                  },
+                  {
+                    offset: 1,
+                    color: 'rgb(255, 70, 131)',
+                  },
+                ]),
+              },
+              data: data,
             },
           ],
         });
