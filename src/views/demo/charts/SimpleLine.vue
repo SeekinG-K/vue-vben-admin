9e89a37dcaf2f0d7b0cc43187b1ef6ee7378fa8c
bo.yan <bo.yan@xuncetech.com>
2021-05-19 19:28:09 +0800

    fix: change symbol style

--- src/views/demo/charts/SimpleLine.vue
+++ src/views/demo/charts/SimpleLine.vue
@@ -8,6 +8,7 @@
   import { useECharts } from '/@/hooks/web/useECharts';
   import type { WeatherInfo } from '/#/api/backstage';
   import echarts from '/@/utils/lib/echarts';
+  import EchartsEnum from '/@/utils/lib/echartsConst';
   export default defineComponent({
     data() {
       return {
@@ -137,21 +138,25 @@
           },
           series: [
             {
-              name: '兴全合宜混合(LOF)A',
               type: 'line',
-              symbol: 'roundRect',
-              sampling: 'lttb',
+              symbol: (rawValue: WeatherInfo, params: Object): string => {
+                debugger;
+                console.log(rawValue);
+                console.log(params);
+                if (rawValue.qty) return EchartsEnum.SYMBOL_BUY_SVG;
+              },
               itemStyle: {
                 color: 'rgb(65, 105, 225)',
+                opacity: 0.618,
               },
               markPoint: {
-                data: [{ type: 'max', name: '最大值', value: '买入' }],
+                data: [{ type: 'max', name: '最大值' }],
                 symbol: (value: Array<Object> | number, params: Object): string => {
-                  debugger;
                   console.log(value);
                   console.log(params);
                   return 'aa';
                 },
+                symbolOffset: [0, '-20%'],
                 label: {
                   show: true,
                   fontSize: 0,
@@ -177,36 +182,6 @@
                 opacity: 0.618,
               },
               data: [20, 80, 60, 80, 20, 15, 60],
-              animation: true,
-              color: 'red',
-            },
-            {
-              name: '创业板指数',
-              type: 'line',
-              symbol: 'roundRect',
-              sampling: 'lttb',
-              itemStyle: {
-                color: 'rgb(65, 105, 225)',
-              },
-              lineStyle: {
-                color: 'rgb(176, 196, 222)',
-                type: 'dashed',
-              },
-              markPoint: {
-                data: [{ type: 'max', name: '最大值', value: '买入' }],
-                symbol: 'circle',
-                label: {
-                  show: true,
-                  fontSize: 0,
-                  color: 'yellow',
-                },
-                symbolSize: 10,
-                animation: true,
-                itemStyle: {
-                  color: 'red',
-                },
-              },
-              data: [10, 70, 40, 20, 10, 20, 80],
               animation: true,
               color: 'red',
             },
